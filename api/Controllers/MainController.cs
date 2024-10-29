0db29bda6296f52402ba45f1ae31ce07d13e8a2d
Felipe-Evaristo <evaristofsantos42@gmail.com>
2024-10-29 00:07:28 -0300

    -feat getUser

--- api/Controllers/MainController.cs
+++ api/Controllers/MainController.cs
@@ -6,10 +6,34 @@ namespace api.Controllers
     [Route("[controller]")]
     public class MainController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult Get()
+
+        [HttpGet("getUser")]
+        public async Task<ActionResult> GetHistorySummonerAsync(string username, string tagName){
+
+        string apiKey = "RGAPI-83880537-447d-42cc-9884-54aa7afbf499"; 
+        string region = "americas"; 
+
+        using (HttpClient client = new HttpClient())
         {
-            return Ok("Hello, summoner!");
+            client.DefaultRequestHeaders.Add("X-Riot-Token", apiKey);
+
+            try
+            {
+                string url = $"https://{region}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{username}/{tagName}?api_key={apiKey}";
+
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                var result = await response.Content.ReadAsStringAsync();
+
+                return Ok(result);
+                // The response will contain a list of match IDs
+            }
+            catch (HttpRequestException e)
+            {
+                return BadRequest($"Erro na solicitação: {e.Message}");
+            }
+        }
         }
     }
 }
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23c79a8af93d6101d8c78f620b07b66ac9c2df2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
